e98bcd318173ed9b00a994088a699052aab79cf3
Francisco Sokol <chico.sokol@gmail.com>
2012-10-19 21:33:08 -0300

    listing projects

--- src/main/java/org/metricminer/refactoringcc/factory/EntryDao.java
+++ src/main/java/org/metricminer/refactoringcc/factory/EntryDao.java
@@ -15,24 +15,54 @@ public class EntryDao {
     }
 
     public List<String[]> list() {
-        try {
-            ArrayList<String[]> res = new ArrayList<String[]>();
-            Statement st = connection.createStatement();
-            String sql = "select * from entry";
-            ResultSet rs = st.executeQuery(sql);
-            int j = 0;
-            while (rs.next()) {
-                String[] row = new String[6];
-                for (int i = 0; i < row.length; i++) {
-                    row[i] = rs.getString(i + 1);
-                }
-                j++;
-                res.add(row);
+        ArrayList<String[]> res = new ArrayList<String[]>();
+        ResultSet rs = executeQuery("select * from entry");
+        int j = 0;
+        while (nextResult(rs)) {
+            String[] row = new String[6];
+            for (int i = 0; i < row.length; i++) {
+                row[i] = getString(rs, i + 1);
             }
-            return res;
+            j++;
+            res.add(row);
+        }
+        return res;
+    }
+
+
+    public List<String> projects() {
+        ArrayList<String> res = new ArrayList<String>();
+        ResultSet rs = executeQuery("select distinct(project_name) from entry");
+        while(nextResult(rs)) {
+            res.add(getString(rs, 1));
+        }
+        return res;
+    }
+
+    private String getString(ResultSet rs, int i) {
+        try {
+            return rs.getString(i);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private boolean nextResult(ResultSet rs) {
+        try {
+            return rs.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ResultSet executeQuery(String sql) {
+        Statement st;
+        try {
+            st = connection.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            return rs;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/test/java/org/metricminer/refactoringcc/factory/EntryDaoTest.java
+++ src/test/java/org/metricminer/refactoringcc/factory/EntryDaoTest.java
@@ -1,14 +1,37 @@
 package org.metricminer.refactoringcc.factory;
 
-import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class EntryDaoTest {
 
-    @Test @Ignore
-    public void test() {
-        EntryDao dao = new EntryDao();
-        dao.list();
+    private EntryDao dao;
+
+    @Before
+    public void setUp() {
+        Connection connection = new ConnectionFactory().openConnection("jdbc:mysql://localhost/refactoring-cc-test");
+        dao = new EntryDao(connection);
     }
+    
+    @Test
+    public void shouldListEntries() {
+        List<String[]> list = dao.list();
+        assertEquals(33625, list.size());
+    }
+    
+    @Test
+    public void shouldListProjects() {
+        List<String> projects = dao.projects();
+        assertEquals(2, projects.size());
+        assertTrue(projects.contains("Apache Ant"));
+        assertTrue(projects.contains("Apache Abdera"));
+    }
+
 
 }
