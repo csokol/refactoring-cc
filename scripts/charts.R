9aaaa678c1bb09de2fffb77354a59a62b446320e
Francisco Sokol <chico.sokol@gmail.com>
2013-04-25 18:59:53 -0300

    charts

--- scripts/charts.R
+++ scripts/charts.R
@@ -21,22 +21,51 @@ withRefactorings = cleanResults[cleanResults$documented.refactorings.total > 0,]
 withRefactorings$refactorings.decreasing.ratio.doc = withRefactorings$documented.refactorings.decreasing.cc / withRefactorings$documented.refactorings.total
 withRefactorings$refactorings.decreasing.ratio.undoc = withRefactorings$undocumented.refactorings.decreasing.cc / withRefactorings$undocumented.refactorings.total
 
-png('plot1.png')
+png('plot.png', width=2*480)
+par(mfrow=c(1,2))
 plot(withRefactorings$total.commits, 
     withRefactorings$refactorings.decreasing.ratio.doc * 100, 
     col="blue", pch=19, ylim=c(0,100), 
     ylab="Percentage of documented refactorings decreasing CC", 
     xlab="Total commit count", 
-    main="Documented refactorings decreasing CC \n versus project commit count")
-dev.off()
-
-
-png('plot2.png')
+    main="(a) Documented refactorings decreasing CC \n versus project commit count")
 plot(withRefactorings$total.commits, 
     withRefactorings$refactorings.decreasing.ratio.undoc * 100, 
     col="blue", pch=19, ylim=c(0,100), 
     ylab="Percentage of common commits decreasing CC", 
     xlab="Total commit count", 
-    main="Common commits decreasing CC \n versus commit count")
+    main="(b) Common commits decreasing CC \n versus commit count")
 dev.off()
+
+print(median(withRefactorings$refactorings.decreasing.ratio.doc) * 100)
+print(median(withRefactorings$refactorings.decreasing.ratio.undoc) * 100)
+
+png('boxplot.png', width=2*480, height=2*480)
+lmts <- range(0,100)
+par(mfrow = c(1, 2))
+boxplot(withRefactorings$refactorings.decreasing.ratio.undoc * 100, ylim=lmts)
+boxplot(withRefactorings$refactorings.decreasing.ratio.doc * 100, ylim=lmts)
+dev.off()
+
+files = dir("../csv/")
+plotIt <- function(name, header) {
+    complete_name = paste("../csv/", name, sep="") 
+    csvfile = read.csv(complete_name, sep=";")
+    png_name = paste("plot-", name, ".png", sep="")
+    png(png_name, width=1.70*480)
+    plot(csvfile, type="n", ylim=c(0, 30000)
+        ylab="Total Cyclomatic Complexity",
+        xlab="Version",
+        main=header)
+    lines(csvfile, col="blue", ylim=c(0, 30000))
+    #text(900, 13800, "(a)", cex=1.5)
+    dev.off()
+}
+
+plotIt("apache-ant.csv", "Apache Ant")
+plotIt("apache-tomcat-7-0-x.csv", "Apache Tomcat 7.0")
+plotIt("apache-camel.csv", "Apache Camel")
+
+
+
 
--- scripts/publica.sh
+++ scripts/publica.sh
@@ -1,7 +1,12 @@
+set -e
 rm *.png;R --save < charts.R 
 
-cp plot2.png ~/ime/mestrado/artigos/msr2013/replication/img/nonrefactorings-decrease.png
-cp plot1.png ~/ime/mestrado/artigos/msr2013/replication/img/refactorings-decrease.png
+#cp plot2.png ~/ime/mestrado/artigos/msr2013/replication/img/nonrefactorings-decrease.png
+#cp plot1.png ~/ime/mestrado/artigos/msr2013/replication/img/refactorings-decrease.png
+cp plot.png ~/ime/mestrado/artigos/msr2013/replication/img/commits-decreasing.png
+cp plot-apache-ant.csv.png ~/ime/mestrado/artigos/msr2013/replication/img/graficos/ant.png
+cp plot-apache-tomcat.csv.png ~/ime/mestrado/artigos/msr2013/replication/img/graficos/tomcat.png
+cp plot-apache-camel.csv.png ~/ime/mestrado/artigos/msr2013/replication/img/graficos/camel.png
 
 cd ~/ime/mestrado/artigos/msr2013/replication/
 make clean
